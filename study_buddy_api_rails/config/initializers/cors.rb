adb1cc5874ec486499ebc71afbcaa6ef17762272
Whitney Smith <whitney.smith.lee@gmail.com>
2020-08-07 13:06:26 -0700

    updated cors

--- study_buddy_api_rails/config/initializers/cors.rb
+++ study_buddy_api_rails/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://compassionate-meitner-9f827a.netlify.app/'
+    origins 'https://compassionate-meitner-9f827a.netlify.app'
 
     resource '*',
       headers: :any,
